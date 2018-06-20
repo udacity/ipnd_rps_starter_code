8f642f88364142356a95e99f302e5b3a53a32887
Zoe Gordin <zoe.gordin@udacity.com>
2018-06-20 16:36:03 -0700

    adding text to readme and starter code file

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ipnd_rps_starter_code
+In this repository you'll find the python file that contains the starter code for the Rock Paper Scissors project. 
---
+++ starter_code.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+moves = ['rock', 'paper', 'scissors']
+
+
+class Player:
+    def move(self):
+        return 'rock'
+
+    def learn(self, my_move, their_move):
+        pass
+
+
+def beats(one, two):
+    return ((one == 'rock' and two == 'scissors') or
+            (one == 'scissors' and two == 'paper') or
+            (one == 'paper' and two == 'rock'))
+
+
+class Game:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def play_round(self):
+        move1 = self.p1.move()
+        move2 = self.p2.move()
+        print(f"Player 1: {move1}  Player 2: {move2}")
+        self.p1.learn(move1, move2)
+        self.p2.learn(move2, move1)
+
+    def play_game(self):
+        print("Game start!")
+        for round in range(3):
+            print(f"Round {round}:")
+            self.play_round()
+        print("Game over!")
+
+
+if __name__ == '__main__':
+    game = Game(Player(), Player())
+    game.play_game()
