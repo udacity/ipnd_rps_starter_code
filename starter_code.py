420aff76956ddcac106c25d6c889db8199262535
Zoe Gordin <zoe.gordin@udacity.com>
2018-07-05 15:54:13 -0700

    added comments

--- starter_code.py
+++ starter_code.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
+"""This program plays a game of Rock, Paper, Scissors between two Players,
+and reports both Player's scores each round."""
+
 moves = ['rock', 'paper', 'scissors']
+
+"""The Player class is the parent class for all of the Players
+in this game"""
 
 
 class Player:
